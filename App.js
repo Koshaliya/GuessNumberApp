4e4533aaf66d3d8c899e0b239eaea5e3f37d4acd
Koshaliya <skoshi36@gmail.com>
2021-06-02 12:28:01 +0530

    font add

--- App.js
+++ App.js
@@ -1,21 +1,71 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './components/Header'
+import StartScreen from './screens/StartGameScreen'
+import GameScreen from './screens/GameScreen'
+import GameOverScreen from './screens/GameOverScreen'
+//import AppLoading from 'expo-app-loading'
+//import Font from 'expo-font'
+
+
+// fetchFonts = () => {
+//   return Font.loadAsync({
+//     'open-sans' : require('./assets/fonts/OpenSans-Regular.ttf'),
+//     'open-sans-bold' : require('./assets/fonts/OpenSans-Bold.ttf')
+//   })
+// }
 
 export default function App() {
+
+  const [userNumber,setUserNumber] = useState();
+  const [guessRound,setGuessRound] = useState(0)
+  const [dataLoaded,setDataLoaded] = useState(false)
+
+  // if(!dataLoaded){
+  //   return (
+  //   <AppLoading 
+  //   startAsync={fetchFonts} 
+  //   onFinish={()=>setDataLoaded(true)}
+  //   onError={(error) => console.log(error)}
+  //   />
+  //   )
+  // }
+
+  const resetGameHandler = () =>{
+    setGuessRound(0)
+    setUserNumber(null)
+  }
+
+  const startGameHandler = (selectedNumber) =>{
+    setUserNumber(selectedNumber);
+    setGuessRound(0)
+  }
+
+  const gameOverHandler = (numofRound) => {
+    setGuessRound(numofRound);
+  }
+
+  let content = <StartScreen onStartGame={startGameHandler}/>
+
+  if(userNumber && guessRound <=0){
+    content=<GameScreen userChoice={userNumber} onGameOver={gameOverHandler}/>
+  }
+  else if(guessRound > 0){
+    content=<GameOverScreen roundNumber={guessRound} guessNumber={userNumber} resetFunc={resetGameHandler}/>
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <Header title='Guess a number'/>
+      {content}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  screen:{
+    flex:1,
+
+  }
 });
---
+++ screens/GameOverScreen.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import {Text,View,StyleSheet,Button} from 'react-native'
+import Color from '../constants/Colors'
+
+const GameOverScreen = props => {
+    return(
+        <View style={styles.screen}>
+            <Text >The Game is Over!!!</Text>
+            <Text>The number of rounds: {props.roundNumber}</Text>
+            <Text>The number entered and guessed was: {props.guessNumber}</Text>
+            <Button title='NEW GAME' onPress={props.resetFunc}></Button>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    screen:{
+        flex:1,
+        alignItems:'center',
+        justifyContent:'center',
+        padding:10
+    },
+})
+
+export default GameOverScreen
